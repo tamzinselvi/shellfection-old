2c3e49af294c132046b6df9b22cd18c7da3afba0
Tom Selvi <tomselvi@gmail.com>
2016-01-25 04:05:34 +0000

    Added a package for tmux, prompt for install as well

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -1,5 +1,5 @@
 # {{{ Install Packages
-for pkg in libevent-dev libncurses-dev pkg-config vim silversearcher-ag cmatrix wget tig pandoc lynx; do
+for pkg in libevent-dev libncurses-dev pkg-config vim silversearcher-ag cmatrix wget tig pandoc lynx automake; do
   printf "$Purple"
   printf "$Divider"
   nl
@@ -25,34 +25,44 @@ done
 # }}}
 
 # {{{ tmux@latest
-if ! which tmux >/dev/null; then
-  printf "$Purple"
-  printf "$Divider"
-  nl
-  textwrap center "Building latest tmux version..."
-  nl
-  printf "$Divider"
-  nl
+printf "$Purple"
+printf "$Divider"
+nl
+textwrap center "Build and install latest tmux version? [y/N]"
+nl
+printf "$Divider"
+printf "$Color_Off"
 
-  sudo apt-get -y install
-  git clone https://github.com/tmux/tmux
-  cd tmux
-  sh autogen.sh
-  ./configure && make
-  sudo mv tmux /usr/bin/tmux
-  cd ..
-  rm -rf tmux
-  printf "$Yellow"
-  nl
-  textwrap center "Finished installing tmux..."
-  nl
-  let Success=Success+1
-else
-  printf "$Green"
-  nl
-  textwrap center "$tmux is already installed, make sure you are on 2.3..."
-  nl
-  let NoChanges=NoChanges+1
-fi
+while true; do
+  eval "printf ' %.0s' {1..$DividerWidthHalf}"
+  read -p "" yn
+  case $yn in
+    [Yy]* )
+      sudo apt-get -y install
+      git clone https://github.com/tmux/tmux
+      cd tmux
+      sh autogen.sh
+      ./configure && make
+      sudo mv tmux /usr/bin/tmux
+      cd ..
+      rm -rf tmux
+      printf "$Yellow"
+      nl
+      textwrap center "Finished installing tmux..."
+      nl
+      let Success=Success+1
+      break;;
+    [Nn]* )
+      printf "$Green"
+      nl
+      textwrap center "Skipping tmux install..."
+      nl
+      let NoChanges=NoChanges+1
+      break;;
+    * ) textwrap center "Please answer yes or no.";;
+  esac
+done
 
+printf "$Divider"
+nl
 # }}}
